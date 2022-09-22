11b0d46a6b1a6c2583c12682ac7a62d408945b0f
metametamoon <sorvinlevs@gmail.com>
2022-09-22 15:08:06 +0300

    Implemented the conversions and interpretation.

---
+++ src/test/kotlin/NfaTest.kt
@@ -0,0 +1,81 @@
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+internal class NfaTest {
+
+    private fun checkNfa(
+        nfa: Nfa,
+        shouldAcceptOn: List<String>,
+        shouldRejectOn: List<String>
+    ) {
+        for (shouldBeAccepted in shouldAcceptOn) {
+            assertTrue(nfa.acceptsString(shouldBeAccepted))
+        }
+        for (shouldBeRejected in shouldRejectOn) {
+            assertFalse(nfa.acceptsString(shouldBeRejected))
+        }
+    }
+
+    companion object {
+        val trivialNfa: Nfa = run {
+            val translationRules = listOf(
+                TranslationRule('a', 0, 1),
+                TranslationRule('b', 1, 2),
+            )
+            Nfa(
+                setOf(element = 0),
+                setOf(element = 2),
+                translationRules
+            )
+        }
+
+        val nfaWithManyInitAccStates = run {
+            val translationRules = listOf(
+                TranslationRule('a', 0, 1),
+                TranslationRule('b', 2, 3),
+            )
+            Nfa(
+                initialStates = setOf(0, 2),
+                acceptingStates = setOf(1, 3),
+                translationRules = translationRules
+            )
+        }
+
+        val nfaWithLoop = run {
+            val translationRules = listOf(
+                TranslationRule('a', 0, 0),
+                TranslationRule('b', 0, 1),
+            )
+            Nfa(
+                setOf(element = 0),
+                setOf(element = 1),
+                translationRules
+            )
+        }
+    }
+
+    @Test
+    fun checkTrivialDfa() {
+        checkNfa(
+            trivialNfa, shouldAcceptOn = listOf("ab"),
+            shouldRejectOn = listOf("aab, ba")
+        )
+    }
+
+    @Test
+    fun checkNfaWithALoop() {
+        checkNfa(
+            nfaWithLoop, shouldAcceptOn = listOf("b", "ab", "aaaab"),
+            shouldRejectOn = listOf("ba, bb", "abb")
+        )
+    }
+
+    @Test
+    fun checkNfaWithManyInitialAndAcceptingStates() {
+        checkNfa(
+            nfaWithManyInitAccStates, shouldAcceptOn = listOf("a", "b"),
+            shouldRejectOn = listOf("ba, bb", "abb")
+        )
+    }
+}
