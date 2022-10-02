afc3d252f60d111ac3e48c8d5a94c04c9b1c5daf
metametamoon <sorvinlevs@gmail.com>
2022-10-03 01:44:54 +0300

    Fixed io problems

--- src/main/kotlin/ReadWrite.kt
+++ src/main/kotlin/ReadWrite.kt
@@ -1,10 +1,14 @@
+fun String.splitBySpace() = split(' ').filter(String::isNotEmpty)
+
 fun readNfaFromFileContent(content: List<String>): Nfa {
-    val n = content[0].toInt()
-    val m = content[1].toInt()
-    val initStates = content[2].split(' ').map(String::toInt).toSet()
-    val accStates = content[3].split(' ').map(String::toInt).toSet()
+    require(content.size >= 5) { "Expected at least 5 lines of input" }
+    @Suppress("UNUSED_VARIABLE") val n = content[0].toInt()
+    @Suppress("UNUSED_VARIABLE") val m = content[1].toInt()
+    val initStates = content[2].splitBySpace().map(String::toInt).toSet()
+    val accStates = content[3].splitBySpace().map(String::toInt).toSet()
     val translationRules = content.subList(4, content.size).map { line ->
-        val tokens = line.split(' ')
+        val tokens = line.splitBySpace()
+        require(tokens.size == 3) { "Expected exactly 3 tokens, but found ${tokens.size} in line \"$line\"" }
         val ch = tokens[1][0]
         val from = tokens[0].toInt()
         val to = tokens[2].toInt()
@@ -18,7 +22,7 @@ fun writeToFile(nfa: Nfa): List<String> {
     val m = nfa.translationRules.map(TranslationRule::char).size
     val initStatesLine = nfa.initialStates.joinToString(" ")
     val accStatesLine = nfa.acceptingStates.joinToString(" ")
-    val translationRulesLines = nfa.translationRules.map { "${it.char} ${it.oldState} ${it.newState}" }
+    val translationRulesLines = nfa.translationRules.map { "${it.oldState} ${it.char} ${it.newState}" }
     return listOf(
         "$n",
         "$m",
---
+++ src/test/kotlin/TestInputOutput.kt
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class TestInputOutput {
+    companion object {
+        fun nfas() = listOf(
+            NfaTest.nfaWithLoop,
+            NfaTest.trivialNfa,
+            NfaTest.nfaWithManyInitAccStates
+        )
+    }
+
+    private fun assertEquals(leftNfa: Nfa, rightNfa: Nfa) {
+        assertEquals(leftNfa.initialStates, rightNfa.initialStates)
+        assertEquals(leftNfa.acceptingStates, rightNfa.acceptingStates)
+        assertEquals(leftNfa.translationRules.toSet(), rightNfa.translationRules.toSet())
+    }
+
+    @Test
+    fun `check input-output consistency`() {
+        for (nfa in nfas()) {
+            assertEquals(nfa, readNfaFromFileContent(writeToFile(nfa)))
+        }
+    }
+
+    @Test
+    fun `throws if too few lines`() {
+        assertThrows<IllegalArgumentException> {
+            readNfaFromFileContent(listOf("2", "4", "2 3"))
+        }
+    }
+
+    @Test
+    fun `throws if not a number`() {
+        assertThrows<Exception> {
+            readNfaFromFileContent(listOf("a", "4", "2 3", "3 1", "1 a 2"))
+        }
+    }
+}
